324fe4f622676f4f3e10147d4f8b8705fbb72582
clayball <cwells73@gmail.com>
2016-05-17 09:09:12 -0400

    adding urlparse library. cleaning up a little.

--- pylinkcheck.py
+++ pylinkcheck.py
@@ -6,9 +6,10 @@
 #  
 # Usage: pylinkcheck.py -r https://www.example.com
 #
-# By default, we can spider and check all of the links found at the URL's domain.
-# For example, a check of https://foo.example.com will only check links with the base
-# URL path of foo.example.com. Link found to bar.example.com will not be checked.
+# By default, we can spider and check all of the links found at the URL's
+# domain. For example, a check of https://foo.example.com will only check
+# links with the base URL path of foo.example.com. Link found to
+# bar.example.com will not be checked.
 # 
 # Fancy run-time options
 #   url root (domain): this is simply required
@@ -22,24 +23,24 @@ import urllib2
 import csv
 from datetime import datetime
 import re
+from urlparse import urlparse
 from bs4 import BeautifulSoup
+
 
 #######################################
 # Functions
 
 # Spider the base URL
 def spiderURL(baseurl, pathlimit):
-	# build a list based on each sub directory found.. this could get a litte hairy
+	# build a list based on each sub directory found
 	print '[spider] path limit set to %d' % pathlimit
 
 
 # Print an informative summary of the dead links
 def printReport(deadlinks):
-	# print each item in the deadlinks list
-	print '\n\n'
-	print '###############################################################################'
-	print ' Link Checker Results'
-	print ' '
+	# print each item in the deadlinks list or CLEAN if empty
+	print '\n\n###############################################################################'
+	print ' Link Checker Results\n'
 	if not deadlinks:
 		print '[+] CLEAN: No dead links found'
 	else:
@@ -51,14 +52,19 @@ def printReport(deadlinks):
 # Main program
 #
 # Get command line options
-parser = argparse.ArgumentParser(description='This is a Python-based link checker.')
-parser.add_argument('-f','--format', help='Output file format ', required=False, default='txt')
-parser.add_argument('-l','--limit', help='Limit directory depth, example.com/limit/dir/depth/', required=False, default=2)
+parser = argparse.ArgumentParser(description='A Python-based link checker.')
+parser.add_argument('-f','--format', required=False, default='txt',
+	help='Output file format ')
+parser.add_argument('-l','--limit', required=False, default=2,
+	help='Limit directory depth, example.com/limit/dir/depth/')
 parser.add_argument('-u','--url', help='Base URL to check', required=True)
 parser.add_argument('-o','--output', help='Output file name', required=False)
 args = parser.parse_args()
 
-# Assign args to vars
+# Assign program arguments to variables
+# - we may want to add a '/' to baseurl if it's not present.
+# - if the href links are relative we need to add the baseurl when checking
+#   the link.
 baseurl = str(args.url)
 pathlimit = int(args.limit)
 
@@ -81,19 +87,40 @@ spiderURL(baseurl, pathlimit)
 
 deadlinks = []
 
+# This for loop will completely change once the spiderURL function is working.
+# We'll iterate over the various directory paths instead.
+
+outofscope = 0
 # Check the URLs
 for link in soup("a"):
-	# only grab the href string
-	#print '[+] found link: %s' % link.get('href')
-
-	# fetch the link but only return the status code
+	# Fetch the link but only return the status code
+	# hrefs are unpredicatable we can add a function to 'clean' them up, i.e.,
+	# get the proto, domain, path, file (TODO: for a complete solution we
+	# need to get all of this)
+	#if baseurl[:-1] == '/':
+	#	print '[debug] strip last char from baseurl'
 	href = link.get('href')
-
-	# build the full URL if the href is relative
-	if re.match('^http', href):
-		checkurl = href
+	print '[debug] href: %s' % href
+	
+	# Separate the file from the path
+	thisurl = urlparse(href)
+	if thisurl.netloc != baseurl and thisurl.netloc != '':
+		print '[-] HREF %s is out of scope' % thisurl.netloc
+		outofscope = 1
 	else:
-		checkurl = baseurl + href
+		print '[debug] path %s' % thisurl.path
+		outofscope = 0
+
+	# Build the full URL if the href is relative.
+	# - assuming, for now, other protocols are not desired
+	# - place this in the Spider function
+	try:
+		if re.match('^http', href):
+			checkurl = href
+		else:
+			checkurl = baseurl + href
+	except:
+		print '[-] Unknown error in re.match()'
 
 	try:
 		#print '[+] checking %s' % checkurl
@@ -101,7 +128,7 @@ for link in soup("a"):
 	except urllib2.HTTPError as e:
 		if e.code == 404:	
 			print '[-] 404 ERROR: %s' % checkurl
-			# add this URL
+			# add this URL to deadlink list
 			deadlinks.append(checkurl)
 		else:
 			print '[-] HTTP ERROR: %d - %s' % (e.code, checkurl)
